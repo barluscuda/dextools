1a6ec5edbe364de6fd2c92cbec9cc8be12f609b4
Barluscuda <barluscuda@gmail.com>
2026-05-13 12:44:40 +0700

    change -> update README.md

--- dextools.go
+++ dextools.go
@@ -1,7 +1,14 @@
 package dextools
 
-import "github.com/barluscuda/dextools/envtools"
+import (
+	"github.com/barluscuda/dextools/envtools"
+	"github.com/barluscuda/dextools/minecraft"
+)
 
 func Env() envtools.EnvTools {
 	return envtools.EnvTools{}
+}
+
+func MinecraftBE() minecraft.Bedrock {
+	return minecraft.Bedrock{}
 }
